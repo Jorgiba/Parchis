af45a41380b402289c0d2e85091e2e61e178320b
Marcos47111 <marcos.jimel@gmail.com>
2026-03-29 11:06:20 +0200

    Main hecho

--- app/src/main/java/com/example/parchis/view/MainActivity.kt
+++ app/src/main/java/com/example/parchis/view/MainActivity.kt
@@ -1,20 +1,37 @@
-package com.example.parchis
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import com.example.parchis.databinding.ActivityMainBinding
+package com.example.parchis.view
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.parchis.R
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var btnIniciarSesion: Button
+    private lateinit var btnIniciarPartida: Button
+    private lateinit var btnRegistrar: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        btnIniciarSesion = findViewById(R.id.btnLogin)
+        btnIniciarPartida = findViewById(R.id.btnGuest)
+        btnRegistrar = findViewById(R.id.btnRegister)
 
+        btnIniciarSesion.setOnClickListener {
+            // Lógica para iniciar sesión
+            startActivity(Intent(this, MainActivity::class.java))
+        }
 
+        btnIniciarPartida.setOnClickListener {
+            // Lógica para iniciar partida como invitado
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        btnRegistrar.setOnClickListener {
+            // Lógica para registrarse
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
-
-
 }
